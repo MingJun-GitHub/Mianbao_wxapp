e3551cc68f23128d271281e37c86f420548b4ade
Season.Li <season.li@watsons.com.cn>
2020-03-23 18:40:40 +0800

    保存图片

--- src/pages/shopCode/index.js
+++ src/pages/shopCode/index.js
@@ -1,4 +1,26 @@
 const app = getApp()
 Page({
-	data: {}
+	data: {},
+	saveCode() {
+		wx.downloadFile({
+			url: 'http://q77wqdcvr.bkt.clouddn.com/shopCode.png', //仅为示例，并非真实的资源
+			success: (res) => {
+				if (res.statusCode === 200) {
+					wx.saveImageToPhotosAlbum({
+						filePath: res.tempFilePath,
+						success(res2) {
+							wx.showToast({
+								title: '保存图片成功！',
+							})
+						},
+						fail(res2) {
+							wx.showToast({
+								title: '保存图片失败！',
+							})
+						}
+					})
+				}
+			}
+		})
+	}
 });
